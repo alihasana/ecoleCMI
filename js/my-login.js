0eaf142a21cd03ce14468d160ac18ec64dfbea0e
mydrivegit <alihasanaoffical@gmail.com>
2018-04-06 22:46:34 +0200

    background and button color changed

--- js/my-login.js
+++ js/my-login.js
@@ -10,7 +10,7 @@ $(function() {
 		});
 		$this.after($("<div/>", {
 			html: 'Show',
-			class: 'btn btn-primary btn-sm',
+			class: 'btn btn-secondary btn-sm',
 			id: 'passeye-toggle-'+i,
 			style: 'position:absolute;right:10px;top:50%;transform:translate(0,-50%);-webkit-transform:translate(0,-50%);-o-transform:translate(0,-50%);padding: 2px 7px;font-size:12px;cursor:pointer;'
 		}));
